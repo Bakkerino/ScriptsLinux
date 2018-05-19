3b569329e072c71dbde4be54544023e3d97467da
Ubuntu <k.m.bakker@st.hanze.nl>
2018-05-19 15:39:53 +0000

    Opmaak pushgit

--- scripts/pushgit.sh
+++ scripts/pushgit.sh
@@ -1,12 +1,12 @@
-echo "#######################################"
-printf "Message: "
+echo "__________________________________________"
+printf "Bericht: "
 read INPUT
 echo "#######################################"
 git add .
 git commit -m "$INPUT"
-echo "########################################"
-echo "Gebruikersnaam en Wachtwoord alstublieft"
-echo "########################################"
+echo " ________________________________________ "
+echo "|Gebruikersnaam en Wachtwoord alstublieft|"
+echo "|________________________________________|"
 git push origin master
 
 
