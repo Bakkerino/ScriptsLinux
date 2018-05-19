137df5ca70766f7302e2ddbdc952eceed724c988
Ubuntu <k.m.bakker@st.hanze.nl>
2018-05-19 15:58:05 +0000

    test

--- scripts/gebruikgit.sh
+++ scripts/gebruikgit.sh
@@ -1,10 +1,14 @@
 echo "_________________________________________"
-read -p "Bericht: " BERICHT 
+read -p "Methode en bericht: " METHODE BERICHT 
+echo " ________________________________________"
+echo "|Methode: $METHODE"
+echo "|Bericht: $BERICHT"
+echo "|________________________________________|"
 git add .
 git commit -m $BERICHT
 echo " ________________________________________ "
 echo "|Gebruikersnaam en Wachtwoord alstublieft|"
 echo "|________________________________________|"
-git push origin master
+git $METHODE origin master
 
 
