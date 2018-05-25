a9e3b974d35dd4f46488fce7b7f4bfbc575e71f7
Ubuntu <k.m.bakker@st.hanze.nl>
2018-05-25 09:39:45 +0000

    photomover

---
+++ scripts/photomover.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+
+echo "Hello, welcome to photoMoverV1. Enjoy your time!"
+
+dir=$1
+
+if [ -d "$dir" ]; then
+echo "This is a valid directory."
+else
+echo "This is not a valid directory, please try again."
+exit
+fi
+
+for i in "$dir"/*
+do
+filename="$i"
+echo $filename
+creationdate=$(ls -l --time-style='+%d-%m-%y' "$i" | awk '{print $6}')
+weeknum$(date --date="$creationdate")
+if [[ $(find . -wholename "$filename"] -mtime +100 -print) ]]; then
+
+mkdir "$dir/$num"
+
+fi
+#copyfile
+cp "$i" "$dir/$num/$filename"
+done
+
+#creationdate=$(ls -l --time-style='+%d-%m-%y' "$i" | awk '{print $6}')
+#weeknum$(date --date="$creationdate")
+
+#if [[ $(find"$filename" -mtime +100 -print) ]]; then
+
+#mkdir "$dir/$num"
+
+#fi
+#copyfile
+#cp "$i" "$dir/$num/$filename"
+
+#check hash
+#originalhash=$(sudo md5sum "$i" | cut -d " " =f1)
+#movedhash=$(sudo md5sum "$dir/$num/$filename" | cut -d " " -f1)
+
+#remove file
+#sudo rm "$i" 
+
+
