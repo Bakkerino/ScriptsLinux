fa93ad51da18a952f0ee03b10bbcac0ebdf84a82
Bakkerino <k.m.bakker@st.hanze.nl>
2018-06-13 11:43:14 +0000

    gitInstallScript

--- scripts/installgit.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install git
-
-git config --global user.name "user_name"
-git config --global user.email "email_id"
-
-git init Mytest
-git remote add origin https://github.com/user_name/Mytest.git
-git push origin master
