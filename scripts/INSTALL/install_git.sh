c14274604d4850af8fce2b98a817c749653baece
Bakkerino <k.m.bakker@st.hanze.nl>
2018-06-13 12:06:32 +0000

    ls

--- scripts/INSTALL/install_git.sh
+++ scripts/INSTALL/install_git.sh
@@ -1,14 +1,21 @@
-apt-get install git
+#Installeert git
+sudo apt-get install git
+#Vraagt variabelen op
 read -p "Gebruikersnaam: " NAAM
 read -p "Email: " EMAIL
 read -p "Github link: " GITLINK
 
-git config --global user.name $NAAM
-git config --global user.email $EMAIL
+#Zet variabelen naam en email weg
+sudo git config --global user.name $NAAM
+sudo git config --global user.email $EMAIL
 echo "####################################"
 echo "Repository wordt aangemaakt in ~/git"
 echo "####################################"
-mkdir ~/git
-git init ~/git
+
+#creeert directory
+sudo mkdir ~/git
+#initialiseert repository
+sudo git init ~/git
+#wisselt van map en koppelt de repository met de git link
 cd ~/git
-git remote add origin $GITLINK
+sudo git remote add origin $GITLINK
