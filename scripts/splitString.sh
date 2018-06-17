078b6345d4d82c3fbb12ffcff9b06da6b9b3f514
Bakkerino <k.m.bakker@st.hanze.nl>
2018-06-17 10:37:05 +0000

    test

--- scripts/splitString.sh
+++ scripts/splitString.sh
@@ -1,6 +1,9 @@
 #Dit scripts is gemaakt om de Hostnaam en ip input te ontleden uit de salt server
 #dit om monitor servers automatisch te deployen met salt
-INPUT="string1: string2"
-OUTPUT="$(cut -d':' -f2 <<< $INPUT)"
 
-echo $OUTPUT
+INPUT="string1: string2 string3: string4"
+#OUTPUT="$(cut -d' '  -f3 <<< $INPUT)"
+for string in $INPUT; do
+	IFS=' ' read -a OUTPUT <<< "${INPUT}"
+	echo ${OUTPUT[0]} | tr -d
+done
