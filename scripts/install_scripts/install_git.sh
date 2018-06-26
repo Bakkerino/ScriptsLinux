c7cb3854b440f6f129e2f4831ee533297bc17fa4
Bakkerino <k.m.bakker@st.hanze.nl>
2018-06-26 11:59:49 +0000

    optioneel vastzetten variabelen

--- scripts/install_scripts/install_git.sh
+++ scripts/install_scripts/install_git.sh
@@ -1,9 +1,14 @@
 #Installeert git
 sudo apt-get install git
-#Vraagt variabelen op
-read -p "Gebruikersnaam: " NAAM
-read -p "Email: " EMAIL
-read -p "Github link: " GITLINK
+#Vraagt variabelen op aan de gebruiker
+#read -p "Gebruikersnaam: " NAAM
+#read -p "Email: " EMAIL
+#read -p "Github link: " GITLINK
+
+#optioneel gebruiksnaam etc vastzetten
+NAAM="bakkerino"
+EMAIL="k.m.bakker@st.hanze.nl"
+GITLINK="https://github.com/Bakkerino/ScriptsLinux.git"
 
 #Zet variabelen naam en email weg
 sudo git config --global user.name $NAAM
@@ -19,3 +24,7 @@ sudo git init ~/git
 #wisselt van map en koppelt de repository met de git link
 cd ~/git
 sudo git remote add origin $GITLINK
+echo "################################"
+echo "Pullen van inhoud git repository"
+echo "################################"
+git pull origin master
